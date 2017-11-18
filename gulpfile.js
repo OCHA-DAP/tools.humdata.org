1035f6b7042e269def716c468cdc22377bc6a75c
Alexandru Artimon <aalecs@gmail.com>
2017-11-18 13:39:28 +0200

    Squashed 'src/hdx-styles/' changes from aa83836..89d2608
    89d2608 Merge pull request #2 from OCHA-DAP/master
    
    git-subtree-dir: src/hdx-styles
    git-subtree-split: 89d26087ef8485fbba100a51fb7ebc2cfc01364f

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
-    less = require('gulp-less'),
-    minifyHTML = require('gulp-htmlmin'),
+    less = require('gulp-less'), 
+    minifyHTML = require('gulp-htmlmin'), 
     jshint = require('gulp-jshint'),
     concat = require('gulp-concat'),
     browserSync = require('browser-sync').create(),
@@ -16,63 +16,31 @@ var gulp = require('gulp'),
     cssimport = require('gulp-cssimport'),
     beautify = require('gulp-beautify'),
     sourcemaps = require('gulp-sourcemaps'),
-    critical = require('critical').stream,
-    argv = require('yargs').argv,
-    gutil = require('gulp-util');
+    critical = require('critical').stream;
 
 /* baseDirs: baseDirs for the project */
 
-// Set production flag
-var isProduction = false;
-gutil.log("Testing out the production flag");
-if (argv.production) {
-    //trigger with gulp --production
-    gutil.log("Production Mode - ON");
-    isProduction = true;
-}
-
 var baseDirs = {
-    dist: 'dist/',
-    src: 'src/',
-    assets: 'dist/assets/',
-    examples: 'dist/examples',
-    hdxStyle: 'src/hdx-styles/src/common/',
-    vendor: 'node_modules/',
-    hxlExample: 'src/hxl-example/**/*'
+    dist:'dist/',
+    src:'src/',
+    assets: 'dist/assets/'
 };
 
 /* routes: object that contains the paths */
 
 var routes = {
     styles: {
-        vendor: [
-            baseDirs.vendor+'select2/dist/css/select2.min.css'
-        ],
-        less: baseDirs.src+'styles/*.less',
-        _less: baseDirs.src+'styles/**/*.less',
+ 		less: baseDirs.src+'styles/*.less',
+        _less: baseDirs.src+'styles/_includes/*.less',
         css: baseDirs.assets+'css/'
     },
 
     templates: {
-        html: baseDirs.src+'templates/**/*'
-    },
-
-    examples: {
-        hxl: baseDirs.examples+'/hxl/'
-    },
-
-    dependencies: {
-
+        html: baseDirs.src+'templates/*.html'
     },
 
     scripts: {
-        vendor: [
-            baseDirs.src+'config/analytics.js',
-            baseDirs.vendor+'jquery/dist/jquery.min.js',
-            baseDirs.vendor+'bootstrap/dist/js/bootstrap.min.js',
-            baseDirs.vendor+'select2/dist/js/select2.min.js'
-        ],
-        base: baseDirs.src+'scripts/',
+        base:baseDirs.src+'scripts/',
         js: baseDirs.src+'scripts/*.js',
         jsmin: baseDirs.assets+'js/'
     },
@@ -80,13 +48,10 @@ var routes = {
     files: {
         html: 'dist/',
         images: baseDirs.src+'images/*',
-        imgmin: baseDirs.assets+'images/',
+        imgmin: baseDirs.assets+'files/img/',
         cssFiles: baseDirs.assets+'css/*.css',
-        htmlFiles: baseDirs.dist+'**/*',
-        styleCss: baseDirs.assets+'css/style.css',
-        hdxStyleFonts: baseDirs.hdxStyle+'fonts/**/*',
-        hdxStyleImages: baseDirs.hdxStyle+'images/**/*',
-        hxlExample: baseDirs.hxlExample+'**/*'
+        htmlFiles: baseDirs.dist+'*.html',
+        styleCss: baseDirs.assets+'css/style.css'
     },
 
     deployDirs: {
@@ -96,44 +61,24 @@ var routes = {
     }
 };
 
-
-/* Production overrides */
-if (isProduction) {
-    routes.scripts.vendor.unshift(baseDirs.src+'config/production.js'); //production config file
-} else {
-    routes.scripts.vendor.unshift(baseDirs.src+'config/default.js'); //main config file
-}
-
 /* Compiling Tasks */
-
 
 // Templating
 
 gulp.task('templates', function() {
     return gulp.src(routes.templates.html)
-        //.pipe(minifyHTML({collapseWhitespace: true}))
+        .pipe(minifyHTML({collapseWhitespace: true}))
         .pipe(browserSync.stream())
-        .pipe(gulp.dest(routes.files.html));
-        // .pipe(notify({
-        //     title: 'HTML minified succesfully!',
-        //     message: 'templates task completed.'
-        // }));
+        .pipe(gulp.dest(routes.files.html))
+        .pipe(notify({
+            title: 'HTML minified succesfully!',
+            message: 'templates task completed.'
+        }));
 });
 
 // SCSS
 
 gulp.task('styles', function() {
-    gulp.src(routes.styles.vendor, {base: '.'})
-        .pipe(plumber({
-            errorHandler: notify.onError({
-                title: "Error: Compiling Vendor CSS.",
-                message:"<%= error.message %>"
-            })
-        }))
-        .pipe(rename('vendor.css'))
-        .pipe(gulp.dest(routes.styles.css))
-        .pipe(browserSync.stream());
-
     return gulp.src(routes.styles.less)
         .pipe(plumber({
             errorHandler: notify.onError({
@@ -146,30 +91,19 @@ gulp.task('styles', function() {
             .pipe(autoprefixer('last 3 versions'))
             .pipe(minifyCss())
         .pipe(sourcemaps.write())
-        // .pipe(cssimport({}))
-        // .pipe(rename('style.css'))
+        .pipe(cssimport({}))
+        .pipe(rename('style.css'))
         .pipe(gulp.dest(routes.styles.css))
-        .pipe(browserSync.stream());
-        // .pipe(notify({
-        //     title: 'Less Compiled and Minified succesfully!',
-        //     message: 'less task completed.',
-        // }));
+        .pipe(browserSync.stream())
+        .pipe(notify({
+            title: 'Less Compiled and Minified succesfully!',
+            message: 'less task completed.',
+        }));
 });
 
 /* Scripts (js) ES6 => ES5, minify and concat into a single file.*/
 
 gulp.task('scripts', function() {
-    gulp.src(routes.scripts.vendor, {base: '.'})
-        .pipe(plumber({
-            errorHandler: notify.onError({
-                title: "Error: Babel and Concat failed.",
-                message:"<%= error.message %>"
-            })
-        }))
-        .pipe(concat('vendor.js'))
-        .pipe(babel())
-        // .pipe(uglify())
-        .pipe(gulp.dest(routes.scripts.jsmin));
     return gulp.src(routes.scripts.js)
         .pipe(plumber({
             errorHandler: notify.onError({
@@ -183,11 +117,11 @@ gulp.task('scripts', function() {
             .pipe(uglify())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(routes.scripts.jsmin))
-        .pipe(browserSync.stream());
-        // .pipe(notify({
-        //     title: 'JavaScript Minified and Concatenated!',
-        //     message: 'your js files has been minified and concatenated.'
-        // }));
+        .pipe(browserSync.stream())
+        .pipe(notify({
+            title: 'JavaScript Minified and Concatenated!',
+            message: 'your js files has been minified and concatenated.'
+        }));
 });
 
 /* Lint, lint the JavaScript files */
@@ -198,33 +132,33 @@ gulp.task('lint', function() {
 			lookup: true,
 			linter: 'jshint',
 		}))
-		.pipe(jshint.reporter('default'));
+		.pipe(jshint.reporter('default')); 
 });
 
 /* Image compressing task */
 
 gulp.task('images', function() {
     gulp.src(routes.files.images)
-        // .pipe(imagemin())
+        .pipe(imagemin())
         .pipe(gulp.dest(routes.files.imgmin));
 });
 
 /* Preproduction beautifiying task (SCSS, JS) */
 
 gulp.task('beautify', function() {
-    // gulp.src(routes.scripts.js)
-    //     .pipe(beautify({indentSize: 4}))
-    //     .pipe(plumber({
-    //         errorHandler: notify.onError({
-    //             title: "Error: Beautify failed.",
-    //             message:"<%= error.message %>"
-    //         })
-    //     }))
-    //     .pipe(gulp.dest(routes.scripts.base))
-    //     .pipe(notify({
-    //         title: 'JS Beautified!',
-    //         message: 'beautify task completed.'
-    //     }));
+    gulp.src(routes.scripts.js)
+        .pipe(beautify({indentSize: 4}))
+        .pipe(plumber({
+            errorHandler: notify.onError({
+                title: "Error: Beautify failed.",
+                message:"<%= error.message %>"
+            })
+        }))
+        .pipe(gulp.dest(routes.scripts.base))
+        .pipe(notify({
+            title: 'JS Beautified!',
+            message: 'beautify task completed.'
+        }));
 });
 
 /* Serving (browserSync) and watching for changes in files */
@@ -233,8 +167,8 @@ gulp.task('serve', function() {
     browserSync.init({
         server: './dist/'
     });
-
-    gulp.watch([routes.styles.less, routes.styles._less], ['styles']);
+    
+    gulp.watch([routes.styles.less, routes.styles._less], ['styles']); 
     gulp.watch(routes.templates.html, ['templates']);
     gulp.watch(routes.scripts.js, ['scripts', 'beautify']);
 });
@@ -254,11 +188,11 @@ gulp.task('uncss', function() {
             })
         }))
         .pipe(minifyCss())
-        .pipe(gulp.dest(routes.styles.css));
-        // .pipe(notify({
-        //     title: 'Project Optimized!',
-        //     message: 'UnCSS completed!'
-        // }));
+        .pipe(gulp.dest(routes.styles.css))
+        .pipe(notify({
+            title: 'Project Optimized!',
+            message: 'UnCSS completed!'
+        }));
 });
 
 /* Extract CSS critical-path */
@@ -280,25 +214,16 @@ gulp.task('critical', function () {
                 message:"<%= error.message %>"
             })
         }))
-        .pipe(gulp.dest(baseDirs.dist));
-        // .pipe(notify({
-        //     title: 'Critical Path completed!',
-        //     message: 'css critical path done!'
-        // }));
+        .pipe(gulp.dest(baseDirs.dist))
+        .pipe(notify({
+            title: 'Critical Path completed!',
+            message: 'css critical path done!'
+        }));
 });
 
-/* Temporary bla */
-gulp.task('hdxAssets', function() {
-    gulp.src(routes.files.hdxStyleFonts)
-    .pipe(gulp.dest(baseDirs.assets + 'fonts/'));
+gulp.task('dev', ['templates', 'styles', 'scripts',  'images', 'serve']);
 
-    return gulp.src(routes.files.hdxStyleImages)
-        .pipe(gulp.dest(baseDirs.assets + 'images/'));
-});
-
-gulp.task('dev', ['templates', 'styles', 'scripts',  'images', 'hdxAssets', 'serve']);
-
-gulp.task('build', ['templates', 'styles', 'scripts', 'images', 'hdxAssets']);
+gulp.task('build', ['templates', 'styles', 'scripts', 'images']);
 
 gulp.task('optimize', ['uncss', 'critical', 'images']);
 
